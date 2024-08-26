c005bc39d8d82adc900767a01548da9d92ab3a00
Aakash Vasant Bhandary <aakashbhandary2024@gmail.com>
2024-08-26 19:21:27 +0000

    added myfile

---
+++ myfile.py
@@ -0,0 +1,17 @@
+def insertionSort(array):
+    size = len(array)
+
+    if size<=1:
+        return
+    
+    for i in range (1, size+1):
+        key = array[i]
+        j = i-1
+        while j >= 0 and key < array[j]:
+            array[j+1] = array[j]
+            j -= 1
+        array[j+1] = key
+
+array = [14, 12, 10, 8, 6, 4, 2, 0, -2]
+insertionSort(array)
+print(array)
